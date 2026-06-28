d05c1398c9cd3ba645970b271336088071ef1332
NickWong123456789 <yangfanwang123@gmail.com>
2026-06-28 18:40:21 +0800

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,5 +1,5 @@
 const NODES = [
-  {name:"阿里云-明月",server:"47.79.251.76",port:51831,uuid:"0f79b4a3-6d8e-4568-a57b-112fb6f94502",net:"tcp"},
+  {name:"阿里云-明月",server:"47.79.251.76",port:443,uuid:"0f79b4a3-6d8e-4568-a57b-112fb6f94502",net:"tcp"},
   {name:"RackNerd-美国",server:"107.172.39.190",port:443,uuid:"4d0e1d5d-dc00-45d8-ae95-493033e095fc",net:"tcp"}
 ];
 
